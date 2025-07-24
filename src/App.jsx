fcb5319c01ab1551b5ad4b38fc89e2fe50ac1c8b
arshavardhan17 <2300031000@kluniversity.in>
2025-07-24 12:40:24 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="bg-blue-500"> Hello World</h1>
+      <div className="w-full h-screen bg-amber-500">1</div>
+      <div className="w-full h-screen bg-green-700">2</div>
+    </div>
+  );
+};
+
+export default App;
