88eff89c0198d14c356d811a21a29bd19d6a6dce
arshavardhan17 <2300031000@kluniversity.in>
2025-07-24 12:47:35 +0000

    Fix numbers in App component for clarity

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ const App = () => {
   return (
     <div>
       <h1 className="bg-blue-500"> Hello World</h1>
-      <div className="w-full h-screen bg-amber-500">11</div>
-      <div className="w-full h-screen bg-green-700">22</div>
+      <div className="w-full h-screen bg-amber-500">1</div>
+      <div className="w-full h-screen bg-green-700">2</div>
     </div>
   );
 };
